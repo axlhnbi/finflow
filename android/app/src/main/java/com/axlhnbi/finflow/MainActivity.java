d8fb5e2feeeed17cc100e70e55a9d0e520d3a28a
axlh <axlhanuebi@gmail.com>
2026-03-17 22:04:12 +0900

    feat: Adjust build

---
+++ android/app/src/main/java/com/axlhnbi/finflow/MainActivity.java
@@ -0,0 +1,5 @@
+package com.axlhnbi.finflow;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.axlhnbi.finflow',
+  appName: 'finflow',
+  webDir: 'www'
+};
+
+export default config;
